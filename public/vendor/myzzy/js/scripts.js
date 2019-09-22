6d4e5833f54b28deb11ec27b01a7aef0833c43e2
Ademir Rocha <tiademir.rocha93@gmail.com>
2019-09-22 20:14:04 -0300

    alteracoes set name system talismã

---
+++ public/vendor/myzzy/js/scripts.js
@@ -0,0 +1,55 @@
+$(document).ready(function (){
+
+// FIXA POSIÇÃO LAYER LINKS
+$(window).scroll(function() {
+	var position = $('#divFixa').position(); 
+	if($(window).scrollTop() >= position.top){
+		$('#divFixa').css('position','fixed'); 
+		$('#divFixa').css('top','0');
+		$('#divFixa').css('border-bottom','1px solid #0080D5');
+		$('#divFixaHidden').css('display','block');
+	} 
+	if($(window).scrollTop() < 110){
+		$('#divFixa').css('position','relative'); 
+		$('#divFixa').css('top','123');
+		$('#divFixa').css('border-bottom','0'); 
+		$('#divFixaHidden').css('display','none');
+	}
+});
+
+// LINK PARA O TOPO DA PÁGINA
+$('.myLinkToTop').click(function(){
+	$('html,body').animate({scrollTop:0},'slow');
+	return false;
+});
+
+// LINKA PARA O TOPO
+// hide #LinkToTop first
+$("#LinkToTop").hide();
+
+// fade in #LinkToTop
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 200) {
+		$('#LinkToTop').fadeIn();
+	} else {
+		$('#LinkToTop').fadeOut();
+	}
+});
+
+// INICIA BX SLIDER
+var slider = $('.ulBxSlider').bxSlider({ });
+$(document).click('.bx-next, .bx-prev',function() { slider.stopAuto(); slider.startAuto(); });
+$(window).resize(function(){ slider.reloadSlider(); });
+
+
+
+
+});
+
+// ATUALIZA CRÉDITOS NO SPAN
+function UpdateCreditos(valCredito){
+	var els = document.getElementsByClassName('spnCreditos');
+	for(var i = 0; i < els.length; i++) {
+		els[i].innerHTML=valCredito;
+	}
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 <head>
 
-	<title>Bolão da Sorte - Loterias Caixa</title>
+	<title>Talismã da Sorte - Loterias Caixa</title>
 	
 	<meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,initial-scale=1"/>
 	<meta charset="utf-8"/>
@@ -10,13 +10,14 @@
 	<meta name="publisher" content="Ademir Rocha / Myzzy Tecnologia" />
 	<meta name="copyright" content="© 2017 Ademir Rocha" />
 	<meta name="robots" content="index,follow" />
-	<meta name="keywords" content="Bolão da Sorte,Loteria online,Megasena" />
-	<meta name="description" content="Bolão da Sorte - Venha tirar a sorte grande." />
-	<link rel="shortcut icon" href="{{asset('vendor/myzzy/images/icons/favicon.ico')}}">
+	<meta name="keywords" content="Talismã da Sorte,Loteria online,Megasena" />
+	<meta name="description" content="Talismã da Sorte - Venha tirar a sorte grande." />
+	<link rel="shortcut icon" href="{{asset('vendor/myzzy/images/icons/favicon.ico')}}" type="image/x-icon">
+	<link rel="icon" href="{{asset('vendor/myzzy/images/icons/favicon.ico')}}" type="image/x-icon">
 
-	<meta property="og:image" content="http://www.loteria-online.com.br/imagens/og-loteria-online.jpg" />
+	<meta property="og:image" content="http://www.talismadasorte.com.br/imagens/og-loteria-online.jpg" />
 	<meta property="og:title" content="Loteria Online" />
-	<meta property="og:url" content="http://www.loteria-online.com.br" />
+	<meta property="og:url" content="http://www.talismadasorte.com.br" />
 	<meta property="og:description" content="" />
 
 	<meta name="csrf-token" content="{{ csrf_token() }}">
@@ -29,84 +30,12 @@
 	<link rel="stylesheet" type="text/css" href="{{asset('vendor/myzzy/css/style.css')}}" />
 	<link rel="stylesheet" type="text/css" href="{{asset('vendor/myzzy/css/bxslider.css')}}" />
 
-
-
-<!--[if lt IE 9]>
-<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-<script src="//css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
-<![endif]-->
-<script type="text/javascript" src="{{asset('vendor/myzzy/js/jquery.1.11.0.min.js')}}"></script>
-<!--<script type="text/javascript" src="scripts/jquery-ui.1.9.1.js" ></script>-->
-<script type="text/javascript" src="{{asset('vendor/myzzy/js/jquery.bxSlider.js')}}"></script>
-<!--<script type="text/javascript" src="scripts/jquery.easing.1.3.js"></script>-->
-<!--<script type="text/javascript" src="scripts/jquery.fancybox-2.1.5.js"></script>
-	<link rel="stylesheet" type="text/css" href="scripts/jquery.fancybox-2.1.5.css" media="screen" />-->
-
 	<link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
 
-
-
-	<script>
-
-		$(document).ready(function (){
-
-// FIXA POSIÇÃO LAYER LINKS
-$(window).scroll(function() {
-	var position = $('#divFixa').position(); 
-	if($(window).scrollTop() >= position.top){
-		$('#divFixa').css('position','fixed'); 
-		$('#divFixa').css('top','0');
-		$('#divFixa').css('border-bottom','1px solid #0080D5');
-		$('#divFixaHidden').css('display','block');
-	} 
-	if($(window).scrollTop() < 110){
-		$('#divFixa').css('position','relative'); 
-		$('#divFixa').css('top','123');
-		$('#divFixa').css('border-bottom','0'); 
-		$('#divFixaHidden').css('display','none');
-	}
-});
-
-// LINK PARA O TOPO DA PÁGINA
-$('.myLinkToTop').click(function(){
-	$('html,body').animate({scrollTop:0},'slow');
-	return false;
-});
-
-// LINKA PARA O TOPO
-// hide #LinkToTop first
-$("#LinkToTop").hide();
-
-// fade in #LinkToTop
-$(window).scroll(function () {
-	if ($(this).scrollTop() > 200) {
-		$('#LinkToTop').fadeIn();
-	} else {
-		$('#LinkToTop').fadeOut();
-	}
-});
-
-// INICIA BX SLIDER
-var slider = $('.ulBxSlider').bxSlider({ });
-$(document).click('.bx-next, .bx-prev',function() { slider.stopAuto(); slider.startAuto(); });
-$(window).resize(function(){ slider.reloadSlider(); });
-
-
-
-
-});
-
-// ATUALIZA CRÉDITOS NO SPAN
-function UpdateCreditos(valCredito){
-	var els = document.getElementsByClassName('spnCreditos');
-	for(var i = 0; i < els.length; i++) {
-		els[i].innerHTML=valCredito;
-	}
-}
-
-</script>
-
+	<script type="text/javascript" src="{{asset('vendor/myzzy/js/jquery.1.11.0.min.js')}}"></script>
+	<script type="text/javascript" src="{{asset('vendor/myzzy/js/jquery.bxSlider.js')}}"></script>
+	<script type="text/javascript" src="{{asset('vendor/myzzy/js/scripts.js')}}"></script>
 
 </head>
 
@@ -143,11 +72,11 @@ function UpdateCreditos(valCredito){
 							<div class="divContent divLinksTop">
 
 								<div class="divLinks alignLeft">
-									<div class="divData">Hoje, 21/09/2019</div>			
+									<div class="divData">Hoje, {{date('d/m/Y')}}</div>			
 								</div>
 
 								<div class="divLinks alignLeft">
-									<a href="" class="aLinkTop" title="Bolões">BOLÕES</a> | <a href="quem-somos" class="aLinkTop" title="Quem somos">QUEM SOMOS</a> | <a href="regras" class="aLinkTop" title="Regras">REGRAS</a>
+									<a href="boloes" class="aLinkTop" title="Bolões">BOLÕES</a> | <a href="quem-somos" class="aLinkTop" title="Quem somos">QUEM SOMOS</a> | <a href="regras" class="aLinkTop" title="Regras">REGRAS</a>
 								</div>
 								
 
@@ -179,7 +108,7 @@ function UpdateCreditos(valCredito){
 
 				<div class="div1Col resp1 alignLeft">
 					<ul class="ulFooter">
-						<li><a href="" title="Bolões">Bolões</a></li>
+						<li><a href="boloes" title="Bolões">Bolões</a></li>
 						<li><a href="quem-somos" title="Quem somos">Quem somos</a></li>
 						<li><a href="regras" title="Regras">Regras</a></li>
 						<li><a href="politicas" title="Políticas">Políticas</a></li>
@@ -190,8 +119,8 @@ function UpdateCreditos(valCredito){
 
 				<div class="div1Col divMarginLeft1 resp1 alignLeft">
 					<ul class="ulFooter">
-						<li><a href="entrar" title="Entrar">Entrar</a></li>
-						<li><a href="cadastro" title="Cadastro">Cadastrar-se</a></li>
+						<li><a href="login" title="Entrar">Entrar</a></li>
+						<li><a href="register" title="Cadastro">Cadastrar-se</a></li>
 						<li><a href="fale-conosco" title="Fale conosco">Fale conosco</a></li>
 						<li><a href="contas" title="Contas">Contas</a></li>
 					</ul>
@@ -203,19 +132,23 @@ function UpdateCreditos(valCredito){
 				<div class="div1Col divMarginLeft2 resp3 marginTop20">
 					<a href="." title="Para o topo"><img src="{{asset('vendor/myzzy/images/logo.png')}}" style="width:150px;max-width:100%;"/></a><br>
 
-					<div class="clearBoth"></div><div class="div3Col"><p>Telefone: (71)&nbsp;98683-5471<br>Contate-nos:&nbsp;<a href="mailto:vendas@sorteprime.com.br" target="_blank">vendas@sorteprime.com.br</a></p>
+					<div class="clearBoth"></div><div class="div3Col"><p>Telefone: (71)&nbsp;98683-5471<br>Contate-nos:&nbsp;<a href="mailto:vendas@talismadasorte.com.br" target="_blank">vendas@talismadasorte.com.br</a></p>
 					</div>	</div>
 
 					<div class="clearBoth"></div>
 
+					<div class="divFooterBottom">
+						<span class="fontFooter">
+							<a href="http://www.myzzy.com.br" title="By Ademir Rocha e Myzzy Tecnologia" target="_blank" class="aFooter">Desenvolvido Por:&nbsp; <img src="{{asset('vendor/myzzy/images/myzzy-footer.png')}}" alt="Myzzy Tecnologia" title="Myzzy Tecnologia" />
+							</a>
+						</span><br>
+					</div>
+				</footer>
 
-				</div>
-				<div class="divFooterBottom">
-					<span class="fontFooter">
-						<a href="http://www.myzzy.com.br" title="By Ademir Rocha e Myzzy Tecnologia" target="_blank" class="aFooter">Desenvolvido por Ademir Rocha e &nbsp; <img src="{{asset('vendor/myzzy/images/myzzy-footer.png')}}" alt="Myzzy Tecnologia" title="Myzzy Tecnologia" /></a></span><br></div>
-					</footer>
+			</div><!-- FECHA DIV WRAPPER -->
+		</div>
+	</div>
 
-				</div><!-- FECHA DIV WRAPPER -->
-			</div>
-		</body>
-		</html>
+	@yield('js')
+</body>
+</html>
